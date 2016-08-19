e1c0cd0e6ca9a5dc796db06ddcce6e1b701dd62c
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-08-19 14:00:27 -0700

    Added typings.json to package output.

--- build-scripts/copydts.js
+++ build-scripts/copydts.js
@@ -1,27 +1,26 @@
 /*eslint no-console:off*/
-const fs = require("fs-extra");
+const fs = require('fs-extra')
 
-const srcDir = "es2015";
-const destDir = "dist";
-const fn = "wcf-date.d.ts";
+const srcDir = 'es2015'
+const destDir = 'lib'
+const fn = 'wcf-date.d.ts'
 
-let src = `${srcDir}/${fn}`;
-let dest = `${destDir}/${fn}`;
-
+let src = `${srcDir}/${fn}`
+let dest = `${destDir}/${fn}`
 
 fs.access(src, (error) => {
-    if (error) {
-        throw new Error(`Source file not found: ${src}`);
-    } else {
-        console.log(`Copying ${src} to ${dest}`);
-        fs.copy(src, dest, {
-            clobber: true,
-            preserveTimestamps: true
-        }, (error) => {
-            if (error) {
-                console.error(`Error copying ${src} to ${dest}`, error);
-                throw error;
-            }
-        });
-    }
-});
+  if (error) {
+    console.error(`Source file not found: ${src}`)
+    throw error
+  } else {
+    console.log(`Copying ${src} to ${dest}`)
+    fs.move(src, dest, {
+      clobber: true
+    }, (error) => {
+      if (error) {
+        console.error(`Error copying ${src} to ${dest}`, error)
+        throw error
+      }
+    })
+  }
+})
