9b63bf536310f0693471212f48f8c3ff53b1750d
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2016-08-31 16:37:39 -0700

    fixed spec JS file for eslint

--- spec/wcf-dateSpec.js
+++ spec/wcf-dateSpec.js
@@ -1,18 +1,18 @@
-/*eslint-env jasmine */
-let wcfDate = require("../lib/wcf-date");
+/* eslint-env jasmine */
+let wcfDate = require('../lib/wcf-date')
 
-describe("wcf-date", function () {
-    const s = "/Date(1408431600000-0700)/";
+describe('wcf-date', function () {
+  const s = '/Date(1408431600000-0700)/'
 
-    it("should be able to parse a WCF date.", function () {
-        let d = wcfDate.parseWcfDate(s);
-        expect(d instanceof Date).toEqual(true);
-    });
+  it('should be able to parse a WCF date.', function () {
+    let d = wcfDate.parseWcfDate(s)
+    expect(d instanceof Date).toEqual(true)
+  })
 
-    it("should be be able to convert a Date to WCF string", function(){
-        let d = wcfDate.parseWcfDate(s);
-        let s2 = wcfDate.toWcfDateString(d);
-        let d2 = wcfDate.parseWcfDate(s2);
-        expect(d.getTime()).toEqual(d2.getTime());
-    });
-});
+  it('should be be able to convert a Date to WCF string', function () {
+    let d = wcfDate.parseWcfDate(s)
+    let s2 = wcfDate.toWcfDateString(d)
+    let d2 = wcfDate.parseWcfDate(s2)
+    expect(d.getTime()).toEqual(d2.getTime())
+  })
+})
